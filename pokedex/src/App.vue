81f88af36240f0c4c3843cdce65434b46f4bc0d1
João Siles <joao.siles@i9xp.com.br>
2021-01-06 18:42:12 -0300

    Introdução a criação do projeto de pokedex usando Vue.js, Axios para Requisições HTTP e Bulma para CSS

---
+++ pokedex/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+        .then(res => {
+          console.log("Pegou a lista de pokemons");
+          this.pokemons = res.data.results;
+          // console.log(this.pokemons);
+        })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
