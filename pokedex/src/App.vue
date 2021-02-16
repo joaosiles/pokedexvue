693ec63d7714d0a30a71c7a9324adb36266ee58f
joaosiles <joaosiles@gmail.com>
2021-02-16 19:37:36 -0300

    Adiciona metodo ao componente e melhora manipulação de dados tornando a aplicação minimamente funcional

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <div v-for="(poke, index) in pokemons" :key="index">
-<!--      <h1>{{ index + 1 }} {{poke.name}}</h1>-->
-      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke, index) in pokemons" :key="index">
+  <!--      <h1>{{ index + 1 }} {{poke.name}}</h1>-->
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
     </div>
-
   </div>
 </template>
 
--- pokedex/src/components/Pokemon.vue
+++ pokedex/src/components/Pokemon.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <h1>{{num}} {{name | upper}}</h1>
-    <small>{{url}}</small>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="currentImg" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{num}} - {{name | upper}}</p>
+            <p class="subtitle is-6">{{pokemon.type}}</p>
+          </div>
+        </div>
+        <div class="content">
+          <button class="button is-medium is-fullwidth" @click="mudarSprite">Mudar Sprite</button>         
+        </div>
+      </div>
+    </div>    
   </div>
 </template>
 
@@ -13,13 +29,20 @@ export default {
     axios.get(this.url).then (res => {
         this.pokemon.type = res.data.types[0].type.name;
         this.pokemon.front = res.data.sprites.front_default;
-        this.pokemon.front = res.data.sprites.back_default;
+        this.pokemon.back = res.data.sprites.back_default;
+        this.currentImg = this.pokemon.front;
     })    
   },
 
   data(){
     return {
-      pokemon: {} // recebe um objeto
+      isFront: true,
+      currentImg: '',
+      pokemon: {
+        type: '',
+        front: '',
+        back: ''
+      } // recebe um objeto
     }
   },
 
@@ -33,10 +56,23 @@ export default {
       var newName = value[0].toUpperCase() + value.slice(1);
       return newName;
     }
+  },
+  methods: {
+    mudarSprite: function(){
+      if(this.isFront){
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      } else{
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
+      }
+    }
   }
 }
 </script>
 
 <style>
-
+  #pokemon{
+    margin-top: 2%;
+  }
 </style>
